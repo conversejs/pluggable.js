1ab01c02a13ed9f568ff06dfa092de29021bbce0
JC Brand <jc@opkode.com>
2016-08-22 08:47:00 +0000

    Improve the docco documentation for pluggable.js

--- pluggable.js
+++ pluggable.js
@@ -10,6 +10,7 @@
 // Pluggable.js enables you to make your Javascript code pluggable while still
 // keeping sensitive objects and data private through closures.
 
+/* Start AMD header */
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         define("pluggable", ["underscore"], factory);
@@ -18,36 +19,40 @@
     }
 }(this, function (_) {
     "use strict";
+/* End AMD header */
 
+    // The `PluginSocket` class contains the plugin architecture, and gets
+    // created whenever `pluggable.enable(obj);` is called on the object
+    // that you want to make pluggable.
+    // You can also see it as the thing into which the plugins are plugged.
+    // It takes two parameters, first, the object being made pluggable, and
+    // then the name by which the pluggable object may be referenced on the
+    // _super object (inside overrides).
     function PluginSocket (plugged, name) {
-        // The PluginSocket class encapsulates the plugin architecture, and gets
-        // created whenver `pluggable.enable(obj);` on the object they want
-        // to make pluggable.
-        // You can also see it as the thing into which the plugins are plugged.
-        this.name = name; // Name by which the now pluggable object may be
-                          // referenced on the _super obj.
+        this.name = name; 
         this.plugged = plugged;
         this.plugged._super = {};
         this.plugins = {};
         this.initialized_plugins = [];
     }
 
+    // Now we add methods to the PluginSocket by adding them to its
+    // prototype.
     _.extend(PluginSocket.prototype, {
-        // Now we add methods to the PluginSocket class by adding them to its
-        // prototype.
 
+        // `wrappedOverride` creates a partially applied wrapper function
+        // that makes sure to set the proper super method when the
+        // overriding method is called. This is done to enable
+        // chaining of plugin methods, all the way up to the
+        // original method.
         wrappedOverride: function (key, value, super_method) {
-            // We create a partially applied wrapper function, that
-            // makes sure to set the proper super method when the
-            // overriding method is called. This is done to enable
-            // chaining of plugin methods, all the way up to the
-            // original method.
             if (typeof super_method === "function") {
                 if (typeof this._super === "undefined") {
-                    // We're not on the context of the plugged object.
-                    // This can happen when the overridden method is called via
-                    // an event handler. In this case, we simply tack on the
-                    // _super obj.
+                    /* We're not on the context of the plugged object.
+                     * This can happen when the overridden method is called via
+                     * an event handler. In this case, we simply tack on the
+                     * _super obj.
+                     */
                     this._super = {};
                 }
                 this._super[key] = super_method.bind(this);
@@ -55,22 +60,22 @@
             return value.apply(this, _.rest(arguments, 3));
         },
 
+        // `_overrideAttribute` overrides an attribute on the original object
+        // (the thing being plugged into).
+        //
+        // If the attribute being overridden is a function, then the original
+        // function will still be available via the `_super` attribute.
+        //
+        // If the same function is being overridden multiple times, then
+        // the original function will be available at the end of a chain of
+        // functions, starting from the most recent override, all the way
+        // back to the original function, each being referenced by the
+        // previous' _super attribute.
+        //
+        // For example:
+        //
+        // `plugin2.MyFunc._super.myFunc => plugin1.MyFunc._super.myFunc => original.myFunc`
         _overrideAttribute: function (key, plugin) {
-            // Overrides an attribute on the original object (the thing being
-            // plugged into).
-            //
-            // If the attribute being overridden is a function, then the original
-            // function will still be available via the _super attribute.
-            //
-            // If the same function is being overridden multiple times, then
-            // the original function will be available at the end of a chain of
-            // functions, starting from the most recent override, all the way
-            // back to the original function, each being referenced by the
-            // previous' _super attribute.
-            //
-            // For example:
-            //
-            // `plugin2.MyFunc._super.myFunc => plugin1.MyFunc._super.myFunc => original.myFunc`
             var value = plugin.overrides[key];
             if (typeof value === "function") {
                 var wrapped_function = _.partial(
@@ -106,6 +111,15 @@
             }.bind(this));
         },
 
+        // Plugins can specify optional dependencies (by means of the
+        // `optional_dependencies` list attribute) which refers to dependencies
+        // which will be initialized first, before the plugin itself gets initialized.
+        // They are optional in the sense that if they aren't available, an
+        // error won't be thrown.
+        // However, if you want to make these dependencies strict (i.e.
+        // non-optional), you can set the `strict_plugin_dependencies` attribute to `true`
+        // on the object being made pluggable (i.e. the object passed to
+        // `pluggable.enable`).
         loadOptionalDependencies: function (plugin) {
             _.each(plugin.optional_dependencies, function (name) {
                 var dep = this.plugins[name];
@@ -134,16 +148,17 @@
             }
         },
 
+        // `applyOverrides` is called by initializePlugin. It applies any
+        // and all overrides of methods or Backbone views and models that
+        // are defined on any of the plugins.
         applyOverrides: function (plugin) {
-            // Called by initializePlugin. Applies any and all overrides
-            // defined on any of the plugins.
             _.each(Object.keys(plugin.overrides || {}), function (key) {
-                // We automatically override all methods and Backbone views and
-                // models that are in the "overrides" namespace.
                 var override = plugin.overrides[key];
                 if (typeof override === "object") {
                     if (typeof this.plugged[key] === 'undefined') {
-                        this.throwUndefinedDependencyError("Error: Plugin \""+plugin.__name__+"\" tried to override "+key+" but it's not found.");
+                        this.throwUndefinedDependencyError(
+                            "Error: Plugin \""+plugin.__name__+
+                            "\" tried to override "+key+" but it's not found.");
                     } else {
                         this._extendObject(this.plugged[key], override);
                     }
@@ -153,12 +168,13 @@
             }.bind(this));
         },
 
+        // `initializePlugin` applies the overrides (if any) defined on all
+        // the registered plugins and then calls the initialize method for each plugin.
         initializePlugin: function (plugin) {
-            // Apply the overrides (if any) defined on all the registered
-            // plugins and then call the initialize method on each one.
             if (_.contains(this.initialized_plugins, plugin.__name__)) {
-                // Don't initialize plugins twice, otherwise we get
-                // infinite recursion in overridden methods.
+                /* Don't initialize plugins twice, otherwise we get
+                 * infinite recursion in overridden methods.
+                 */
                 return;
             }
             _.extend(plugin, this.properties);
@@ -172,16 +188,19 @@
             this.initialized_plugins.push(plugin.__name__);
         },
 
+        // `registerPlugin` registers (or inserts, if you'd like) a plugin,
+        // by adding it to the `plugins` map on the PluginSocket instance.
         registerPlugin: function (name, plugin) {
-            // Register (or insert) a plugin, but adding it to the `plugins`
-            // map on the PluginSocket instance.
             plugin.__name__ = name;
             this.plugins[name] = plugin;
         },
 
+        // `initializePlugins` should get called once all plugins have been
+        // registered. It will then iterate through all the plugins, calling
+        // `initializePlugin` for each.
+        // The passed in  properties variable is an object with attributes and methods
+        // which will be attached to the plugins.
         initializePlugins: function (properties) {
-            // The properties variable is an object of attributes and methods
-            // which will be attached to the plugins.
             if (!_.size(this.plugins)) {
                 return;
             }
@@ -190,16 +209,17 @@
         }
     });
     return {
+        // Call the `enable` method to make an object pluggable
+        //
+        // It takes three parameters:
+        // - `object`: The object that gets made pluggable.
+        // - `name`: The string name by which the now pluggable object
+        //     may be referenced on the _super obj (in overrides).
+        //     The default value is "plugged".
+        // - `attrname`: The string name of the attribute on the now
+        //     pluggable object, which refers to the PluginSocket instance
+        //     that gets created.
         'enable': function (object, name, attrname) {
-            // Call this method to make the passed in object pluggable
-            // It takes three parameters:
-            // - object: The object that gets made pluggable.
-            // - name: The string name by which the now pluggable object
-            //     may be referenced on the _super obj (in overrides).
-            //     The default value is "plugged".
-            // - attrname: The string name of the attribute on the now
-            //     pluggable object, which refers to the PluginSocket instance
-            //     that gets created.
             if (typeof attrname === "undefined") {
                 attrname = "pluginSocket";
             }
